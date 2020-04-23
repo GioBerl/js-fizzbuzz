cf417c699540aa9fe4b2f5736e67cc4424fd9106
Gio <giovanni.berl1@gmail.com>
2020-04-23 08:01:54 +0200

    cambio let var

--- buzz.js
+++ buzz.js
@@ -3,13 +3,13 @@
 // al posto dei multipli di 5 stampi "Buzz"
 // al posto dei multipli sia di 3 che di 5 stampi "FizzBuzz"
 
-for (let i = 1; i < 101; i++) {
+for (var i = 1; i < 101; i++) {
     if (i % 15 == 0) {
-        console.log(i, 'FizzBuzz');
+        console.log(i, "FizzBuzz");
     } else if (i % 5 == 0) {
-        console.log(i, 'Buzz');
+        console.log(i, "Buzz");
     } else if (i % 3 == 0) {
-        console.log(i, 'Fizz');
+        console.log(i, "Fizz");
     } else {
         console.log(i, i);
     }
